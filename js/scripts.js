5ef6bbc578c7a00b2847da13cb6bd45d20e3102d
Roberta B Ferreira <robertabs.ferreira@gmail.com>
2024-04-19 00:04:33 -0300

    Update Scripts
    First javascript update. Added button's events. Screen text is working =D

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+const previousOperationText = document.querySelector("#previous-operation")
+const currentOperationText = document.querySelector("#current-operation")
+const buttons = document.querySelectorAll("#buttons-container button");
+
+class Calculator {
+    constructor(previousOperationText, currentOperationText){
+        this.previousOperationText = previousOperationText
+        this.currentOperationText = currentOperationText
+        this.currentOperation = ""
+    }
+// add digit to calculator screen
+    addDigit(digit) {
+        this.currentOperation = digit
+        this.updateScreen()
+    }
+
+// change values of the calculator screen
+updateScreen (){
+    this.currentOperationText.innerText += this.currentOperation;
+}
+
+}
+
+const calc = new Calculator(previousOperationText, currentOperationText);
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const value = e.target.innerText;
+
+        if(+value >= 0 || value === "."){
+            calc.addDigit(value);
+        } else {
+            console.log("Op: "+ value);
+        }
+    });
+});
